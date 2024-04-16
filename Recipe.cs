00d9377ad9aa55b486f772b883e133967561b007
AbdulDavids <abdulbaaridavids04@gmail.com>
2024-04-16 17:45:38 +0200

    recipe added

--- Recipe.cs
+++ Recipe.cs
@@ -6,7 +6,56 @@ using System.Threading.Tasks;
 
 namespace PROG6221POE
 {
-    internal class Recipe
+    public class Recipe
     {
+        public List<Ingredient> Ingredients { get; set; }
+        public List<string> Steps { get; set; }
+
+        public Recipe()
+        {
+            Ingredients = new List<Ingredient>();
+            Steps = new List<string>();
+        }
+
+        public void AddIngredient(Ingredient ingredient)
+        {
+            Ingredients.Add(ingredient);
+        }
+
+        public void AddStep(string step)
+        {
+            Steps.Add(step);
+        }
+
+        public void DisplayRecipe()
+        {
+            Console.WriteLine("Ingredients:");
+            foreach (var ingredient in Ingredients)
+            {
+                Console.WriteLine($"- {ingredient}");
+            }
+
+            Console.WriteLine("\nSteps:");
+            int stepNumber = 1;
+            foreach (var step in Steps)
+            {
+                Console.WriteLine($"{stepNumber}. {step}");
+                stepNumber++;
+            }
+        }
+
+        public void ScaleRecipe(double factor)
+        {
+            foreach (var ingredient in Ingredients)
+            {
+                ingredient.Quantity *= factor;
+            }
+        }
+
+        public void ResetQuantities(List<Ingredient> originalIngredients)
+        {
+            Ingredients = new List<Ingredient>(originalIngredients);
+        }
     }
+
 }
