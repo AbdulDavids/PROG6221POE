b4c54675f696369b76b8b1ca32750f6b55dcc9d7
AbdulDavids <abdulbaaridavids04@gmail.com>
2024-04-16 17:44:57 +0200

    ingredient class added

---
+++ Ingredient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG6221POE
+{
+    public class Ingredient
+    {
+        public string Name { get; set; }
+        public double Quantity { get; set; }
+        public string UnitOfMeasurement { get; set; }
+
+        public Ingredient(string name, double quantity, string unitOfMeasurement)
+        {
+            Name = name;
+            Quantity = quantity;
+            UnitOfMeasurement = unitOfMeasurement;
+        }
+
+        public override string ToString()
+        {
+            return $"{Quantity} {UnitOfMeasurement} of {Name}";
+        }
+    }
+
+}
