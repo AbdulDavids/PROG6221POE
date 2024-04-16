169db8d0346173cd680d23c01695530692dc971b
AbdulDavids <abdulbaaridavids04@gmail.com>
2024-04-16 17:50:09 +0200

    fix private -> public current recipe

--- RecipeManager.cs
+++ RecipeManager.cs
@@ -8,7 +8,7 @@ namespace PROG6221POE
 {
     public class RecipeManager
     {
-        private Recipe currentRecipe;
+        public Recipe currentRecipe;
         private List<Ingredient> originalIngredients;
 
         public RecipeManager()
